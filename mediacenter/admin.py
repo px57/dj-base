146aa9e3e4509ea8a27c35d934a0dffc77b11717
projeta618 <projeta618@gmail.com>
2023-10-07 23:51:36 -0300

    Update the form element

--- mediacenter/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import FilesModel, FilesPermission
-
-@admin.register(FilesModel)
-class FilesModelAdmin(admin.ModelAdmin):    
-    list_display = ('id', 'profile', 'file_name', 'src', 'md5', 'label')
-    list_filter = ('profile', 'file_name', 'src', 'md5', 'label')
-    search_fields = ('profile', 'file_name', 'src', 'md5', 'label')
-
