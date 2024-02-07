0a3949bdda036dbc8e7d9cabaff011cec09f3b63
projeta618 <projeta618@gmail.com>
2024-02-07 20:22:11 -0300

    feat: Add kernel module
    This commit adds the `kernel` module to the project. The `kernel` module is a shared library dedicated to Django that contains a collection of small libraries aimed at simplifying and improving the work.
    
    Changes:
    - Added `kernel/README.md` file.
    - Added `kernel/SUMMARY.md` file.
    - Added `kernel/__init__.py` file.
    - Added `kernel/__pycache__/__init__.cpython-310.pyc` file.

---
+++ kernel/interfaces/stack.py
@@ -0,0 +1,29 @@
+
+
+class RulesStack:
+    """
+        @description: Il s'agit ici d'une pile de règles, d'interface.
+    """
+
+    def __init__(self) -> None:
+        """
+            @description: 
+        """
+        self.rules = {
+
+        } 
+
+    def set_rule(self, ruleClass):
+        """
+            @description: This function sets the rule 
+        """
+        self.rules[ruleClass.label] = ruleClass
+
+    def get_rule(self, interface_name: str):
+        """
+            @description: Get the rule or raise an exception.
+        """
+        if interface_name in self.rules:
+            return self.rules[interface_name]
+        
+        raise Exception('The rule with the interface_name: ' + interface_name + ' does not exist')
