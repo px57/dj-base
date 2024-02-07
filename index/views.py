9cae8b00cdc3f3df88dd16528ded848766256a2c
projeta618 <projeta618@gmail.com>
2024-02-07 20:21:27 -0300

    chore: Add necessary files for the "index" module
    Explanation:
    - This commit adds the necessary files for the "index" module in the project.
    
    Changes:
    - Deleted the submodule entry for "index" in the parent index file.
    - Added a new README.md file in the "index" directory.
    - Added a new "__init__.py" file in the "index" directory.
    - Added "__pycache__" directory with ".pyc" files for different Python versions.
    -

---
+++ index/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from index.decorators import load_languages
+from profiles import libs as profile_libs
+import os
+
+@load_languages
+def index(request, path=None):
+    # response = HttpResponse('blah')
+    # profile_libs.generate_anonymous_user(request, response)
+
+    return render(request, request.language.get_index_src())
